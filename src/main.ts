8504e55a086c18e10ddbaf1b425a8df6e0c3f756
JuliaJau <jautzj@gmail.com>
2021-08-06 12:02:21 +0200

    Add Headline an Search

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', { innerText: 'Animal Crossing: New Horizons' }),
+    createElement('input', { placeholder: 'Search for a character' }),
+  ],
+});
+
 if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  `;
+  app.append(mainElement);
 }
