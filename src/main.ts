db10bf3a4afd690ac83937bacd21ee8f3b1ac000
JuliaJau <jautzj@gmail.com>
2021-08-06 14:21:26 +0200

    ✅ Import function createCharacterCard to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import './style.css';
 import { createElement } from './utils/createElement';
+import { createCharacterCard } from './components/character/character';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 const mainElement = createElement('main', {
   childElements: [
     createElement('h1', { innerText: 'Animal Crossing: New Horizons' }),
-    createElement('input', { placeholder: 'Search for a character' }),
+    createElement('p', { innerText: 'Fish Characters' }),
+    createElement('input', {
+      placeholder: 'Search for a character',
+      className: 'searchbar',
+    }),
+    createElement('div', {
+      className: 'characterContainer',
+      childElements: [createCharacterCard()],
+    }),
   ],
 });
 
